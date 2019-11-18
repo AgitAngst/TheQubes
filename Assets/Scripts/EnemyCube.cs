2aa1b372f68f8ff944e76d2bce1d652783283ac6
AgitAngst <markiz.de.sad@gmail.com>
2019-11-18 19:26:23 +0300

    Init
    Added DOTwin and test qubes

---
+++ Assets/Scripts/EnemyCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCube : CubeMoveAndRotate
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.tag = "Enemy";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer();
+    }
+}
---
+++ Assets/Scripts/FriendlyCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendlyCube : CubeMoveAndRotate
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.tag = "Friend";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer();
+    }
+}
