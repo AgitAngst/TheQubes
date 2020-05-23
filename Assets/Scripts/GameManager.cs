bad1b9478cba2ce7a5a4286a2086dd5543dbb78b
AgitAngst <markiz.de.sad@gmail.com>
2020-05-23 17:04:31 +0300

    Floor to parent
    Floor cube are generate in parent.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,7 +20,7 @@ public class GameManager : MonoBehaviour {
     public int cubesCountX = 10;
     public int cubesCountZ = 10;
     public int cubesFloorCountZ = 15;
-
+    public float ggridSpacingOffsetFloor = 1f;
     public float girdSpacingOffset = 1f;
     public int greenCubeNum = 5;
 
@@ -32,7 +32,11 @@ public class GameManager : MonoBehaviour {
 
     public GameObject floorCube;
 
+    public List<GameObject> gameObjectsArr = new List<GameObject>();
+    public List<GameObject> floorArr = new List<GameObject>();
 
+
+    int cloneCounter = 0;
     GameObject tmpSpawn;
     Vector3 pos;
     public Vector3 grdOrigin = Vector3.zero;
@@ -83,20 +87,40 @@ public class GameManager : MonoBehaviour {
     }
 
     void GenerateFloorCubes () {
+        GameObject floor = new GameObject("Floor");
         for (int x = 0; x < cubesCountX; x++) {
             for (int z = 0; z < cubesFloorCountZ; z++) {
-                Vector3 spawnPosition = new Vector3 (x * girdSpacingOffset, -1, z * girdSpacingOffset) + grdOrigin;
-                SpawnFloorCubes (spawnPosition, Quaternion.identity);
+                Vector3 spawnPosition = new Vector3 (x * ggridSpacingOffsetFloor, -1, z * ggridSpacingOffsetFloor) + grdOrigin;
+                GameObject clone = Instantiate(floorCube, spawnPosition, Quaternion.identity);
+                floorArr.Add(clone);
             }
+        }
+
+        foreach (var item in floorArr)
+        {
+            item.transform.parent = floor.transform;
+
         }
     }
 
     void SpawnCubes (Vector3 positionToSpawn, Quaternion rotationToSpawn, CubeColor color) {
         GameObject clone;
-
+        int count = 0;
         switch (color) {
             case CubeColor.Red:
-                clone = Instantiate (cubeToSpawnRed, positionToSpawn, rotationToSpawn);
+                GameObject newGroup = new GameObject("TestGroup");
+
+                if (count < 4)
+                {
+                    count++;
+                    gameObjectsArr.Add(Instantiate(cubeToSpawnRed, positionToSpawn, rotationToSpawn));
+                    
+                }
+                foreach (GameObject item in gameObjectsArr)
+                {
+                    item.transform.parent = newGroup.transform;
+                }
+                
                 break;
 
             case CubeColor.Green:
@@ -108,9 +132,7 @@ public class GameManager : MonoBehaviour {
                 break;
         }
     }
-    void SpawnFloorCubes (Vector3 positionToSpawn, Quaternion rotationToSpawn) {
-        GameObject clone = Instantiate (floorCube, positionToSpawn, rotationToSpawn);
-    }
+
 
     
 }
